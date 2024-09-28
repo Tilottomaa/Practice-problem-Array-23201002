896d2fd6db52bc149c25fe4d88e9c8abf1f9d0fd
Tilottomaa <tilottomatilka5@gmail.com>
2024-09-28 22:49:18 +0600

    Sort.an.array.c

--- Sort.an.array.c
+++ Sort.an.array.c
@@ -8,15 +8,14 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
+    for(i=0;i<n-1;i++){
+	for(j=i+1;j<n;j++){
+		if(arr[j]>arr[i]){
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+	}}
     printf("Sorted array : \n");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
