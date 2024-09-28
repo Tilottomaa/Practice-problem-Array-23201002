0126b3bccdef7e51358711c5e0b1d65b046eb11f
Tilottomaa <tilottomatilka5@gmail.com>
2024-09-28 23:29:43 +0600

    Move-all-negative.c

--- Move-negative.c
+++ Move-negative.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int n;
+    int n,i,j,temp;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
     int arr[n];
@@ -8,18 +8,18 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    int i = 0, j = 0;
-    while (j < n) {
-        if (arr[j] < 0) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-        }
-        j++;
+     for(i=0;i<n-1;i++){
+	for(j=i+1;j<n;j++){
+		if(arr[j]<arr[i]){
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+	}}
+
+for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    for (int k = 0; k < n; k++) {
-        printf("%d ", arr[k]);
-    }
+
     return 0;
-}
+    }
